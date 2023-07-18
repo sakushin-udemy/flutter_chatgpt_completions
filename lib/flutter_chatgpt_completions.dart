99479bdee3143b80499448ec176cd7f5e316207a
sakushin-udemy <sakushin-udemy@gmail.com>
2023-07-18 19:45:57 +0900

    実行時のエラーを解消した

--- lib/flutter_chatgpt_completions.dart
+++ lib/flutter_chatgpt_completions.dart
@@ -4,3 +4,4 @@ export 'package:flutter_chatgpt_completions/src/chat.dart';
 export 'package:flutter_chatgpt_completions/src/chatgpt/logic/chatgpt_chat.dart';
 
 export 'package:flutter_chatgpt_completions/src/chatgpt/models/chat_data.dart';
+export 'package:flutter_chatgpt_completions/src/chatgpt/models/user_chat_message.dart';
--- lib/src/chat.dart
+++ lib/src/chat.dart
@@ -1,8 +1,10 @@
+import 'chatgpt/models/chat_message.dart';
+
 /// チャットインターフェースを定義する抽象クラスです。
 ///
 /// この抽象クラスは、チャット機能を実装するために使用されます。
 /// 継承して具体的なチャット機能を実装してください。
-abstract class Chat<T> {
+abstract class Chat<T extends ChatMessage> {
   /// ユーザーからのメッセージに対して応答を生成します。
   ///
   /// [message] ユーザーからのメッセージ。
